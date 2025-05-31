6f392cdca857b6ccfb495d44fec989351f8f7741
Yutong Zhao <SylviaChiu@outlook.com>
2025-05-31 12:11:13 +0100

    Tiny improvement on Utils.hpp

--- libs/Utils.hpp
+++ libs/Utils.hpp
@@ -1,3 +1,6 @@
+#ifndef microhedger_utilities_general_hpp
+#define microhedger_utilities_general_hpp
+
 #include <iostream>
 #include <vector>
 
@@ -6,6 +9,8 @@ class Utils
 public:
     Utils() {}
     ~Utils() {}
-    
+
     static void sortPairedVectors(std::vector<double> &A, std::vector<double> &B);
 };
+
+#endif
