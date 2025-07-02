867ceb2b97f0d717e5523868f83b5fb5ed554fa5
Yutong Zhao <SylviaChiu@outlook.com>
2025-07-02 16:24:41 +0800

    Added functionalities for comparative statics

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
     const int H = 5;
     const int Q = 4;
     const int total_time = T * H * Q;
-    const int seed = 9999;
+    const int seed = 1;
     const int n_samples = 1000;
 
     const double p0 = 5.0;
@@ -17,25 +17,37 @@ int main()
     const std::vector<double> bps0 = {4.94, 4.96, 4.98};
     const std::vector<double> bvs0(bps0.size(), 10.0);
     const double decay_coefficient = 0.00;
+
     const LOB lob0(decay_coefficient, aps0, avs0, bps0, bvs0);
     const double vol_news = 0.0;
     const double order_arrival_intensity = 1.0;
     const double p_otype = 0.1; // prob of getting market orders
-    const double p_info = 0.3; // prob of getting informed orders
+    const double p_info = 0.3;  // prob of getting informed orders
     const double vol_min = 0;
     const double vol_max = 1;
     const double m_spr = -0.1;
     const double v_spr = 0.1;
-    const double option_pos = 80;
+    const double option_pos = 0;
     const double implied_vol = 0.089;
+    const double tick_size = 0.01;
+
+    Bar::SetTickSize(tick_size);
 
     PathInfo pi(T, H, Q, p0, lob0, option_pos, implied_vol);
     RandomInfo ri(seed, vol_news, order_arrival_intensity,
-              p_otype, p_info, vol_min, vol_max, m_spr, v_spr, 0.5);
+                  p_otype, p_info, vol_min, vol_max, m_spr, v_spr, 0.5);
 
-    PathCollection paths(n_samples, pi, ri);
-    paths.GeneratePaths();
-    paths.PrintSimulationResults();
+    Parameter param_type = N_DAYS;
+    const std::vector<double> param_range = {5, 6, 7, 8, 9};
+
+    std::vector<PathInfo> pi_scenarios;
+    PathInfo::GenerateScenarios(pi_scenarios, param_type, param_range, pi);
+    for (auto &pi_scen : pi_scenarios)
+    {
+        PathCollection paths(n_samples, pi_scen, ri);
+        paths.GeneratePaths();
+        paths.PrintSimulationResults();
+    }
 
     // std::vector<int> normal_paths_id;
     // paths.FindPathsWithStatus(0, normal_paths_id);
